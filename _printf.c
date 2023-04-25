78fb2c073f7178fb021c3ff171a22aa9b4970395
ghieth98 <hossamghieth@gmail.com>
2023-04-25 06:27:56 -0700

    first and second task

---
+++ _printf.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * _printf - function that prints output
+ * @format: character string with 0 to 3 directives
+ * Return: the number of characters to be printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list valist;
+	int i, buffend = 0;
+	double totalBuffer = 0;
+	double *total;
+	char *holder;
+	char buffer[BUFSIZE];
+	char *(*spec_func)(va_list) = NULL;
+
+	if (!format)
+		return (-1);
+	va_start(valist, format);
+	total = &totalBuffer;
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			spec_func = get_spec_func(format[i]);
+			holder = (spec_func) ? spec_func(valist) : nothing_found(format[i]);
+			if (holder)
+				buffend = alloc_buffer(holder, _strlen(holder), buffer, buffend, total);
+		}
+		else
+		{
+			holder = chartos(format[i]);
+			buffend = alloc_buffer(holder, 1, buffer, buffend, total);
+		}
+	}
+	_puts(buffer, buffend);
+	va_end(valist);
+	return (totalBuffer + buffend);
+}
---
+++ get_spec_func.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * get_spec_func - check specifier function and return function
+ * @s: type to check
+ * Return: pointer to function to use
+ */
+
+char *(*get_spec_func(char s))(va_list)
+{
+	int i;
+	s_types spec_types[] = {
+		{"c", char_find},
+		{"s", string_find},
+		{"%", percent_find},
+		{"d", int_find},
+		{"i", int_find},
+
+	};
+
+	for (i = 0; spec_types[i].spec; i++)
+		if (s == *spec_types[i].spec)
+			return (spec_types[i].f);
+
+	return (spec_types[i].f);
+}
